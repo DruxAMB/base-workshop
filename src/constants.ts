145edf8a8634debce0011271c676a01647557d51
Muhammad Amoo <druxambmarketing@gmail.com>
2024-09-12 09:56:45 +0100

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const BASE_SEPOLIA_CHAIN_ID = 84532;
+export const mintContractAddress = '0x11b4128A343d9aB02bED83050c952f383EAb6B24';
+export const mintABI = [
+  {
+    inputs: [],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const;
